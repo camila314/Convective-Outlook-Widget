30a33b3bab55f076f357edb064724972f51098d9
camila314 <47485054+camila314@users.noreply.github.com>
2026-05-24 12:49:30 -0500

    Initial Commit

---
+++ Convective Outlook Widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Convective Outlook Widget
+//
+//  Created by Full Name on 5/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Convective Outlook Widget/Convective_Outlook_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Convective_Outlook_WidgetApp.swift
+//  Convective Outlook Widget
+//
+//  Created by Full Name on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Convective_Outlook_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
